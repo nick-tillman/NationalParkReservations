666094f92e8eb575f99711b042b70f19dba98c3d
Hannah Dixie <h.dixie.06@gmail.com>
2018-10-25 10:01:08 -0400

    park and campground pojo

--- capstone/src/main/java/com/techelevator/model/Campground.java
+++ capstone/src/main/java/com/techelevator/model/Campground.java
@@ -2,4 +2,48 @@ package com.techelevator.model;
 
 public class Campground {
 
+	private long campgroundId;
+	private int parkId;
+	private String name;
+	private String openFromMonth;
+	private String openToMonth;
+	private double dailyFee;
+	
+	
+	public long getCampgroundId() {
+		return campgroundId;
+	}
+	public void setCampgroundId(long campgroundId) {
+		this.campgroundId = campgroundId;
+	}
+	public int getParkId() {
+		return parkId;
+	}
+	public void setParkId(int parkId) {
+		this.parkId = parkId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getOpenFromMonth() {
+		return openFromMonth;
+	}
+	public void setOpenFromMonth(String openFromMonth) {
+		this.openFromMonth = openFromMonth;
+	}
+	public String getOpenToMonth() {
+		return openToMonth;
+	}
+	public void setOpenToMonth(String openToMonth) {
+		this.openToMonth = openToMonth;
+	}
+	public double getDailyFee() {
+		return dailyFee;
+	}
+	public void setDailyFee(double dailyFee) {
+		this.dailyFee = dailyFee;
+	}
 }
