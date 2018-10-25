57d43445c6a8f4381607d2b0d5752dde07c5d725
Hannah Dixie <h.dixie.06@gmail.com>
2018-10-25 16:33:04 -0400

    park and campground updates

--- capstone/src/main/java/com/techelevator/model/jdbc/JDBCCampgroundDAO.java
+++ capstone/src/main/java/com/techelevator/model/jdbc/JDBCCampgroundDAO.java
@@ -1,25 +1,52 @@
 package com.techelevator.model.jdbc;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.model.Campground;
 import com.techelevator.model.CampgroundDAO;
 
 public class JDBCCampgroundDAO implements CampgroundDAO {
 	
-	
+	private JdbcTemplate jdbcTemplate;
 
-	@Override
-	public List<Campground> getAllCampgroundsInPark(long parkId) {
-		// TODO Auto-generated method stub
-		return null;
+	public JDBCCampgroundDAO(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
 	@Override
-	public List<Campground> getAllCampgroundsWithDate(LocalDate fromDate, LocalDate toDate) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Campground> getAllCampgroundsInPark(long parkId) {
+		
+		List<Campground> allCampgroundsByParkId = new ArrayList<Campground>();
+		
+		String sqlReturnCampgroundsByParkId = "SELECT * FROM campground WHERE park_id = ?; ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlReturnCampgroundsByParkId, parkId);
+		
+		while(results.next()) {
+			Campground newCampground = mapRowToCampground(results);
+			
+			allCampgroundsByParkId.add(newCampground);
+		}
+		
+		return allCampgroundsByParkId;
+		
+	}
+	
+	private Campground mapRowToCampground(SqlRowSet results) {
+		Campground newCampground = new Campground();
+		newCampground.setCampgroundId(results.getLong("campground_id"));
+		newCampground.setParkId(results.getInt("park_id"));
+		newCampground.setName(results.getString("name"));
+		newCampground.setOpenFromMonth(results.getString("open_from_mm"));
+		newCampground.setOpenToMonth(results.getString("open_to_mm"));
+		newCampground.setDailyFee(results.getDouble("daily_fee"));
+		return newCampground;
+		
 	}
 
 }
