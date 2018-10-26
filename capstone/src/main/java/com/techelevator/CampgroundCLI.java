923a694d3ef6982e19ba52a24bf1f2c0723c875a
Nick Tillman <ndt6281@gmail.com>
2018-10-26 09:40:03 -0400

    made some progress

--- capstone/src/main/java/com/techelevator/CampgroundCLI.java
+++ capstone/src/main/java/com/techelevator/CampgroundCLI.java
@@ -4,8 +4,30 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
-public class CampgroundCLI {
+import com.techelevator.model.jdbc.JDBCCampgroundDAO;
+import com.techelevator.model.jdbc.JDBCParkDAO;
+import com.techelevator.model.jdbc.JDBCReservationDAO;
+import com.techelevator.model.jdbc.JDBCSiteDAO;
 
+public class CampgroundCLI {
+	
+	private static final String MENU_OPTION_RETURN_TO_PREVIOUS_SCREEN = "Return to Previous Screen";
+	
+	private static final String MENU_PARK_OPTIONS_VIEW_CAMPGROUNDS = "View Campgrounds";
+	private static final String MENU_PARK_OPTIONS_SEARCH_RESERVATION = "Search for Reservation";
+	private static final String[] MENU_PARK_OPTIONS = new String[] {MENU_PARK_OPTIONS_VIEW_CAMPGROUNDS,
+																	MENU_PARK_OPTIONS_SEARCH_RESERVATION,
+																	MENU_OPTION_RETURN_TO_PREVIOUS_SCREEN};
+	
+	private static final String VIEW_CAMPGROUND_OPTIONS_SEARCH_RESERVATION = "Search for Available Reservation";
+	private static final String[] VIEW_CAMPGROUND_OPTIONS = new String[] {VIEW_CAMPGROUND_OPTIONS_SEARCH_RESERVATION, 
+																		MENU_OPTION_RETURN_TO_PREVIOUS_SCREEN};
+	
+	private JDBCCampgroundDAO campgroundDAO;
+	private JDBCParkDAO parkDAO;
+	private JDBCReservationDAO reservationDAO;
+	private JDBCSiteDAO siteDAO;
+	
 	public static void main(String[] args) {
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setUrl("jdbc:postgresql://localhost:5432/campground");
@@ -17,7 +39,11 @@ public class CampgroundCLI {
 	}
 
 	public CampgroundCLI(DataSource datasource) {
-		// create your DAOs here
+		campgroundDAO = new JDBCCampgroundDAO(datasource);
+		parkDAO = new JDBCParkDAO(datasource);
+		reservationDAO = new JDBCReservationDAO(datasource);
+		siteDAO = new JDBCSiteDAO(datasource);
+		
 	}
 	
 	public void run() {
